36dbd407e5d52c586ce85825e5c317468a6a34a0
Leopold <mr.leopold.k@gmail.com>
2019-12-05 23:56:32 +0200

    Correct in task 6 p.1
    Add Corrections to Task 6 part 1

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -12,10 +12,9 @@ namespace Task_6
         {
             Console.WriteLine("Part 1");
             var persBase = PersonsService.GetPersons();
-            string a = "Finn" + "Mertens" + true + 14;
+            var a = new { FirstName = persBase.Select(x=>x.FirstName), LastName = persBase.Select(x => x.LastName), Gender = persBase.Select(x=>x.Gender), Age = persBase.Select(x=>x.Age) };
             var pers = persBase.Select(PersonDescription => new { a });
             
-
             Console.WriteLine("Part 2");
             var persBase2 = PersonsService.GetPersons();
             var pers2 = persBase2.Where(x => x.Age < 100).GroupBy(x => x.Gender).GroupBy(group => group.Count()).Select(r => new { r.Key, у = r.Count(), e = r});
